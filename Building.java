412506fba03c0084aa16898921944a77f1018d8f
Egor Bryantsev <egor.bryantsev1@gmail.com>
2025-06-03 01:36:41 +0200

    add basic building class

---
+++ Building.java
@@ -0,0 +1,51 @@
+package Citysimulator;
+
+import javax.swing.ImageIcon;
+
+public class Building {
+    private String name;
+    private ImageIcon imageIcon;
+    private int cost;
+    // happiness, energy, etc
+
+    public Building(String name, String imagePath, int cost) {
+        this.name = name;
+        this.cost = cost;
+        // add image scaling
+        ImageIcon originalIcon = new ImageIcon(getClass().getResource(imagePath));
+        this.imageIcon = new ImageIcon();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ImageIcon getImageIcon() {
+        return this.imageIcon;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+
+    public boolean build(Building[][] gameGrid, int row, int column) {
+        // maybe add check for if row/column exist
+        if (gameGrid[row][column] == null) {
+            gameGrid[row][column] = this;
+            return true;
+        } else {
+            System.out.println("Cell (" + row + ", " + column + ") is already occupied.");
+            return false;
+        }
+    }
+
+    public Building delete(Building[][] gameGrid, int row, int column) {
+        Building removedBuilding = gameGrid[row][column];
+        if (removedBuilding != null) {
+            gameGrid[row][column] = null;
+            return removedBuilding; //output deleted building
+        } else {
+            return null;
+        }
+    }
+}
