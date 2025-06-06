3991afa69267b513bcde5c0c93a685b8301b5ab5
Egor Bryantsev <egor.bryantsev1@gmail.com>
2025-06-06 23:42:20 +0200

    fix args for house constructor

--- src/main/java/com/Group57/House.java
+++ src/main/java/com/Group57/House.java
@@ -1,14 +1,16 @@
 package com.Group57;
 
+import javax.swing.*;
 import java.awt.Color;
 
 public class House extends Building {
     public static final String HOUSE_NAME = "House";
+    private static final String HOUSE_IMAGE_PATH = "/house.png";
     public static final int HOUSE_COST = 25;
     public static final Color HOUSE_COLOR = Color.ORANGE;
 
     public House() {
-        super(HOUSE_NAME, HOUSE_COST, HOUSE_COLOR);
+        super(HOUSE_NAME, HOUSE_IMAGE_PATH, HOUSE_COST);
     }
 
     @Override
